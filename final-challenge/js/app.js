e6a47445f61ec32864a883d28a8a0dc80e780f7e
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-03-01 14:16:24 +0100

    Header (Navbar) and Hero section

---
+++ final-challenge/js/app.js
@@ -0,0 +1,7 @@
+// Carousel
+const myCarouselElement = document.querySelector('#hero-carousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
