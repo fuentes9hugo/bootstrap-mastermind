72d3375563afced69b07890bde62acc4cd4f908c
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-03-01 21:07:11 +0100

    Completed Home page

--- final-challenge/js/app.js
+++ final-challenge/js/app.js
@@ -5,3 +5,7 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
+//PopOver
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
